47a770e0aa9b6b3f0a28d50e43c17c48ca6e7bc3
Igor Olejar <igor.olejar@gmail.com>
2026-04-09 09:29:26 +0100

    Main logic

---
+++ main.js
@@ -0,0 +1,30 @@
+const cron = require('node-cron');
+const fs = require('fs');
+const { checkSite } = require('./monitor');
+const { sendEmailReport } = require('./mailer');
+const { timeStamp } = require('console');
+
+const SITES = ['https://www.google.com', 'https://www.facebook.com', 'https://www.twitter.com'];
+const DB_PATH = './data.json';
+
+function saveResult(result) {
+    const data = JSON.parse(fs.readFileSync(DB_PATH, 'utf-8') || '[]');
+    data.push({ ...result, timeStamp: new Date() });
+    fs.writeFileSync(DB_PATH, JSON.stringify(data, null, 2));
+}
+
+cron.schedule('0 * * * *', async () => {
+    for (const site of SITES) {
+        const siteStatus = await checkSite(site);
+        saveResult(siteStatus);
+    }
+});
+
+cron.schedule('0 9 * * *', async () => {
+    const data = JSON.parse(fs.readFileSync(DB_PATH, 'utf-8'));
+
+    if (data.length > 0) {
+        await sendEmailReport(data);
+        fs.writeFileSync(DB_PATH, JSON.stringify([]));
+    }
+});
